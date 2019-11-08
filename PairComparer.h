40b6aa5a84f95e54d248e1cb621385984c3d2654
amt954 <42959019+amt954@users.noreply.github.com>
2019-11-08 12:57:05 -0800

    Add files via upload

---
+++ PairComparer.h
@@ -0,0 +1,31 @@
+#ifndef PAIR_COMPARER_H
+#define PAIR_COMPARER_H
+
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+class MaxHeapPairComparer
+{
+public:
+	template<typename K, typename V>
+	bool operator()(const pair<K, V>& left, const pair <K, V>& right)
+	{
+		return left.second < right.second;
+	}
+};
+
+class MinHeapPairComparer
+{
+
+public:
+	template<typename K, typename V>
+	bool operator()(const pair<K, V>& left, const pair <K, V>& right)
+	{
+		return left.second > right.second;
+	}
+};
+
+#endif
+
