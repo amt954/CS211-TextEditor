9a0eaac9682c2de6133d2f60749c4b5383548a13
amt954 <42959019+amt954@users.noreply.github.com>
2019-11-08 12:55:42 -0800

    Update binary.h

--- binary.h
+++ binary.h
@@ -3,22 +3,26 @@
 
 #include <string>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <iostream>
 #include <cstdlib>
 #include <sstream>
 #include <vector>
+#include <queue>
+#include "PairComparer.h"
 
 using namespace std;
 
-void findFrequency(vector <string> vecOfStrings)
+priority_queue< pair<char, int>, vector<pair<char, int>>, MaxHeapPairComparer> max_pq{};
+
+void findFrequency(vector <char> vecOfStrings)
 {
-	map<std::string, int> countMap;
+	unordered_map<char, int> countMap;
 
 	//Iterate over the vector and store the frequency of each element in map
 	for (auto& elem : vecOfStrings)
 	{
-		auto result = countMap.insert(pair<string, int>(elem, 1));
+		auto result = countMap.insert(pair<char, int>(elem, 1));
 		if (result.second == false)
 			result.first->second++;
 	}
@@ -26,6 +30,7 @@ void findFrequency(vector <string> vecOfStrings)
 	// Iterate over the map
 	for (auto& elem : countMap)
 	{
+		max_pq.push(make_pair(elem.first, elem.second));
 		cout << elem.first << ": " << elem.second << endl;
 	}
 }
